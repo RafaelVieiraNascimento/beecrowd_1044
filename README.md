6a214aeb54a3ab5925a870939bb4c2479a0b5b1c
RafaelVN <contato@RafaelVN.com>
2022-08-10 11:02:47 -0300

    beecrowd_1044

---
+++ README.md
@@ -0,0 +1,13 @@
+# beecrowd_1044
+
+beecrowd | 1044 - Múltiplos
+
+Adaptado por Neilor Tonin, URI  Brasil
+
+Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", indicando se os valores lidos são múltiplos entre si.
+
+Entrada
+A entrada contém valores inteiros.
+
+Saída
+A saída deve conter uma das mensagens conforme descrito acima.
---
+++ src/program/Main.java
@@ -0,0 +1,29 @@
+package program;
+
+/*
+beecrowd | 1044 - Múltiplos
+
+Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem
+"Sao Multiplos" ou "Nao sao Multiplos", indicando se os valores lidos são múltiplos entre si.
+
+Entrada
+A entrada contém valores inteiros.
+
+Saída
+A saída deve conter uma das mensagens conforme descrito acima.
+*/
+
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		if(A % B == 0 || B % A == 0) System.out.println("Sao Multiplos");
+		else System.out.println("Nao sao Multiplos");
+		sc.close();
+	}
+}
